23974e8635829407ae70c01ffbaee9a77f44ad90
Mehdi Ali <ali-mehdi@live.de>
2019-04-01 18:18:36 +0200

    Implement test for loading trained TransE model that was trained on SG4MR

---
+++ tests/test_load_sg4mr_model_01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Test the instantiation of TransE trained on SG4MR"""
+
+import unittest
+
+from tests.utils import load_model
+
+"""Note: Test cun be run only if GPU is avilable since model was trained on GPU."""
+
+class TestModelInstantiation(unittest.TestCase):
+
+    def test_instantation(self):
+        model_direc = '../scholarly_data_related_recommendations/SG4MR/sg4mr_model_01'
+        transE_compath_model = load_model(model_directory = model_direc)
+
+        self.assertIsNotNone(transE_compath_model)
